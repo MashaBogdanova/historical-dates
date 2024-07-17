890372d8a777d7506673c1be8983a93b643a40f0
Maria Bogdanova <masha.bogdanoff@gmail.com>
2024-07-17 14:45:16 +0200

    feat: add title to markup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Dates from "@/app/components/dates/Dates";
 export default function Home() {
     return (
         <main className={styles.main}>
+            <h1 className={styles.main__title}>Исторические<br/>даты</h1>
             <Lines/>
             <Dates/>
         </main>
