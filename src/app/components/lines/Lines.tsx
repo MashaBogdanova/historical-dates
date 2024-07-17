4b4481a532aaa3eb5097f5c9b8597e75ef77f7a9
Maria Bogdanova <masha.bogdanoff@gmail.com>
2024-07-17 13:39:49 +0200

    feat: add lines to markup

---
+++ src/app/components/lines/Lines.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from "./lines.module.scss";
+
+function Lines() {
+    return (
+        <section className={styles.lines}>
+            <div className={styles.lines__colorBorder}/>
+            <div className={`${styles.lines__line} ${styles.lines__line_horizontal}`}/>
+            <div className={`${styles.lines__line} ${styles.lines__line_vertical}`}/>
+        </section>
+    );
+}
+
+export default Lines;
