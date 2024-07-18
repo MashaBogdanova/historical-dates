2a4aee9cf2397578f38b7302dc880fa6bcb9eec4
Maria Bogdanova <masha.bogdanoff@gmail.com>
2024-07-18 15:39:13 +0200

    feat: add slider to markup

---
+++ src/app/components/slider/Slider.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {Navigation} from 'swiper/modules';
+import 'swiper/scss';
+import 'swiper/css/navigation';
+import {Period} from "@/mocks/dates-mock";
+import styles from "./slider.module.scss";
+import React from "react";
+
+export default function Slider({period}: Period) {
+    const years = Object.keys(period.years);
+
+    return (
+        <section className={styles.slider}>
+            <Swiper
+                modules={[Navigation]}
+                spaceBetween={80}
+                slidesPerView={3}
+                navigation
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                {years.map((year) => {
+                    return (
+                        <SwiperSlide key={year}>
+                            <h4 className={styles.slider__title}>{year}</h4>
+                            <p className={styles.slider__text}>{period.years[year]}</p>
+                        </SwiperSlide>
+                    )
+                })}
+            </Swiper>
+        </section>
+    );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,12 @@ import Circle from "@/app/components/circle/Circle";
 import Lines from "@/app/components/lines/Lines";
 import Dates from "@/app/components/dates/Dates";
 import Pagination from "@/app/components/pagination/Pagination";
+import Slider from "@/app/components/slider/Slider";
+import {dates} from "@/mocks/dates-mock";
+
 
 export default function Home() {
+
     return (
         <main className={styles.main}>
             <h1 className={styles.main__title}>Исторические<br/>даты</h1>
@@ -12,6 +16,7 @@ export default function Home() {
             <Circle/>
             <Dates/>
             <Pagination/>
+            <Slider period={dates[0]}/>
         </main>
     );
 }
