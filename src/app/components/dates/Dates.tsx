d1def5ed04eaae45683aa1e925710e38949e87a7
Maria Bogdanova <masha.bogdanoff@gmail.com>
2024-07-19 12:33:12 +0200

    feat: change dates by one

--- src/app/components/dates/Dates.tsx
+++ src/app/components/dates/Dates.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
+'use client';
+
+import React, {useEffect, useState} from 'react';
 import styles from "./dates.module.scss";
 
 interface Props {
     start: number;
     end: number;
 }
-const Dates = ({startDate, endDate}: Props) => {
+export default function Dates ({startDate, endDate}: Props) {
+
+    const [displayStartDate, setDisplayStartDate] = useState(startDate);
+    const [displayEndDate, setDisplayEndDate] = useState(endDate);
+
+    useEffect(() => {
+        const updateValue = (currentValue, targetValue, setValue) => {
+            if (currentValue !== targetValue) {
+                const increment = currentValue < targetValue ? 1 : -1;
+                setTimeout(() => setValue(currentValue + increment), 15);
+            }
+        };
+
+        const intervalId = setInterval(() => {
+            updateValue(displayStartDate, startDate, setDisplayStartDate);
+            updateValue(displayEndDate, endDate, setDisplayEndDate);
+        }, 50);
+
+        return () => clearInterval(intervalId);
+    }, [startDate, endDate, displayStartDate, displayEndDate]);
+
     return (
         <section className={styles.dates}>
-            <h2 className={styles.dates__start}>{startDate}</h2>
-            <h2 className={styles.dates__end}>{endDate}</h2>
+            <h2 className={styles.dates__start}>{displayStartDate}</h2>
+            <h2 className={styles.dates__end}>{displayEndDate}</h2>
         </section>
     );
 };
-
-export default Dates;
